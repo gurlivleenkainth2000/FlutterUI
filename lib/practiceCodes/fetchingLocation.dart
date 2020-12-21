d457fb278dd7073d313e50b699ffc8f8b5c330a5
Gurlivleen Singh Kainth <gurlivleen.kainth2000@gmail.com>
2020-12-21 23:05:30 +0530

    Fetching User Address Line using the combination of location and geoCoder packages simultaneously.

--- lib/practiceCodes/fetchingLocation.dart
+++ lib/practiceCodes/fetchingLocation.dart
@@ -1,6 +1,6 @@
-// import 'dart:html';
 import 'package:flutter/material.dart';
 import 'package:location/location.dart';
+import 'package:geocoder/geocoder.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 fetchCurrentLocation() async {
@@ -28,6 +28,11 @@ fetchCurrentLocation() async {
   return _locationData;
 }
 
+fetchUserAddress(Coordinates coordinates) async {
+  var address = await Geocoder.local.findAddressesFromCoordinates(coordinates);
+  return address.first;
+}
+
 class UserAddressPage extends StatefulWidget {
   @override
   _UserAddressPageState createState() => _UserAddressPageState();
@@ -35,6 +40,7 @@ class UserAddressPage extends StatefulWidget {
 
 class _UserAddressPageState extends State<UserAddressPage> {
   LocationData locationData;
+  String mainAddress = "Not Known";
   String longitude = "Not Known";
   String latitude = "Not Known";
 
@@ -45,47 +51,65 @@ class _UserAddressPageState extends State<UserAddressPage> {
         title: Text("Location"),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Your Location', style: Theme.of(context).textTheme.headline1,),
-            RichText(
-              text: TextSpan(
-              children: <TextSpan>[
-                TextSpan(
-                    text: 'Latitude: \t',
-                    style: GoogleFonts.openSans(fontSize: 18.0,color: Colors.grey[600],)
-                ),
-                TextSpan(
-                    text: latitude,
-                    style: GoogleFonts.openSans(
-                        fontSize: 18.0,
-                        fontWeight: FontWeight.w600,
-                        color: Colors.grey[800]
-                    )
-                ),
-              ]
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Your Location', style: Theme.of(context).textTheme.headline1,),
+              RichText(
+                text: TextSpan(
+                children: <TextSpan>[
+                  TextSpan(
+                      text: 'Latitude: \t',
+                      style: GoogleFonts.openSans(fontSize: 18.0,color: Colors.grey[600],)
+                  ),
+                  TextSpan(
+                      text: latitude,
+                      style: GoogleFonts.openSans(
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.grey[800]
+                      )
+                  ),
+                ]),
               ),
-            ),
-            RichText(
-              text: TextSpan(
-              children: <TextSpan>[
-                TextSpan(
-                    text: 'Longitude: \t',
-                    style: GoogleFonts.openSans(fontSize: 18.0,color: Colors.grey[600],)
-                ),
-                TextSpan(
-                    text: longitude,
-                    style: GoogleFonts.openSans(
-                        fontSize: 18.0,
-                        fontWeight: FontWeight.w600,
-                        color: Colors.grey[800]
-                    )
-                ),
-              ]
+              RichText(
+                text: TextSpan(
+                children: <TextSpan>[
+                  TextSpan(
+                      text: 'Longitude: \t',
+                      style: GoogleFonts.openSans(fontSize: 18.0,color: Colors.grey[600],)
+                  ),
+                  TextSpan(
+                      text: longitude,
+                      style: GoogleFonts.openSans(
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.grey[800]
+                      )
+                  ),
+                ]),
               ),
-            ),
-          ],
+              RichText(
+                text: TextSpan(
+                    children: <TextSpan>[
+                      TextSpan(
+                          text: 'Address: \t',
+                          style: GoogleFonts.openSans(fontSize: 18.0,color: Colors.grey[600],)
+                      ),
+                      TextSpan(
+                          text: mainAddress,
+                          style: GoogleFonts.openSans(
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.w600,
+                              color: Colors.grey[800]
+                          )
+                      ),
+                    ]),
+              ),
+            ],
+          ),
         )
       ),
       floatingActionButton: FloatingActionButton(
@@ -94,10 +118,13 @@ class _UserAddressPageState extends State<UserAddressPage> {
         tooltip: 'Get Current Location',
         onPressed: () async {
           locationData = await fetchCurrentLocation();
-          print(">>> "+ locationData.toString());
+          Coordinates coordinates = new Coordinates(locationData.latitude, locationData.longitude);
+          Address address = await fetchUserAddress(coordinates);
+          print(">>> " + address.toString());
           setState(() {
             longitude = locationData.longitude.toString();
             latitude = locationData.latitude.toString();
+            mainAddress = address.addressLine.toString();
           });
         },
       ),
